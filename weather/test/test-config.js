0b9cd56c5cef5668cd95f2a6770270058aceff56
TheBlurOSX <TheBlurOSX@MacBook-Air-de-TheBlurOSX.local>
2016-02-23 17:31:57 +0100

    best version

--- weather/test/test-config.js
+++ weather/test/test-config.js
@@ -1,9 +1,21 @@
 var assert = require('assert'),
     config = require('./../lib/config');
 
+var units = {
+        props1: 1,
+        props2: 2
+    },
+    ip = '8908';
+
+var Config = config(units, ip);
+
 
 describe('Config', function () {
-    it('has 3 props')
+    it('has 3 props', function()
+    {
+      assert(Object.keys(Config).length === 3); //if we have 3 args expressions on the terminal
+    });
+
 
     it('after construction, 2 props are populated')
 
